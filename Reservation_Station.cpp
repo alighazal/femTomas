fe5e72cc1fa1c6f81441a41ecb0520cb81662cf0
Ali Ghazal <alighazal@aucegypt.edu>
2019-12-07 10:50:25 +0200

    reservarion station -- classes

--- Reservation_Station.cpp
+++ Reservation_Station.cpp
@@ -1,1 +1,13 @@
 #include "Reservation_Station.h"
+
+Reservation_Station::Reservation_Station()
+{
+	busy = false;
+	type = inst_type::ADD;
+	Vj = -1;
+	Vk = -1 ;
+	Qj = -1;
+	Qk = -1;
+	Dest = -1;
+	A = 0;
+}
