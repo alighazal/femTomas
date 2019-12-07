277ff39005e4fe5c10f0fd3a75293ad4bcfff0c0
Ali Ghazal <alighazal@aucegypt.edu>
2019-12-07 13:53:13 +0200

    new pars

--- Instruction_Status.h
+++ Instruction_Status.h
@@ -2,6 +2,8 @@
 
 #include <string>
 
+enum class status {issued, dispatched, executed, committed };
+
 using namespace std;
 
 class Instruction_Status
@@ -9,11 +11,12 @@ class Instruction_Status
 private:
 	string inst;
 	int rs1, rs2, rd, A;
-	int issued, 
+	int issued, dispacthed, executed;
+	status current_status;
 
 
 
-public:
+
 
 
 };
